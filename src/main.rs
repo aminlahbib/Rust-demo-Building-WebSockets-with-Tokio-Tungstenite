f3d889e542f5d0ebe08d6a0d9e2f430f44b91b46
Amin Lahbib <amine.lhb00@gmail.com>
2025-11-04 05:04:28 +0100

    sending and receiving msgs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use futures_util::{SinkExt, StreamExt};
 use tokio_tungstenite::connect_async;
+use tokio_tungstenite::tungstenite::Message;
+
 
 #[tokio::main]
 async fn main() {
@@ -7,4 +10,8 @@ async fn main() {
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     println!("WebSocket connection established");
+
+    let (mut write, mut read) = ws_stream.split();
+    let msg = Message::text("Hello, echo server");
+    write.send(msg).await.expect("Failed to send");
 }
