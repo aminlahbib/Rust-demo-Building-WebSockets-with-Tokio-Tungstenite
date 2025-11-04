518255218e336c6eceda6d5c6f08ea65f53c5324
Amin Lahbib <amine.lhb00@gmail.com>
2025-11-04 05:11:20 +0100

    reading msgs

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,20 @@ async fn main() {
     println!("WebSocket connection established");
 
     let (mut write, mut read) = ws_stream.split();
+
     let msg = Message::text("Hello, echo server");
+
+    if let Some(message) = read.next().await{
+        let message = message.expect("Failed to read message");
+        println!("Message received: {}", message);
+    }
+
+    println!("Sending message: {}", msg);
     write.send(msg).await.expect("Failed to send");
+
+    if let Some(message) = read.next().await{
+        let message = message.expect("Failed to read message");
+        println!("Message received: {}", message);
+    }
+
 }
