5be2f8dd0534e629f56f2309745d79e4687ec57e
Amin Lahbib <amine.lhb00@gmail.com>
2025-11-04 04:52:04 +0100

    DNS lookup failure: WebSocket echo server chchanged to wss://ws.ifelse.io

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use tokio_tungstenite::connect_async;
 
 #[tokio::main]
 async fn main() {
-    let url: &str = "wss://echo.websocket.events";
+    let url: &str = "wss://ws.ifelse.io";
     println!("Connecting to {}", url);
 
     let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-    println!("WebSocket connection established"); 
+    println!("WebSocket connection established");
 }
